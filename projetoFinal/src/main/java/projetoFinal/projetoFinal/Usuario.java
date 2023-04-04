0109b6868152ddb4eda80c57af4c1aef123797ff
wfilhosb <wfilhosb@hotmail.com>
2023-04-03 21:16:35 -0300

    Criacao dos campos cod nomeCompleto e email, e seus respectivos metodos.

---
+++ projetoFinal/src/main/java/projetoFinal/projetoFinal/Usuario.java
@@ -0,0 +1,32 @@
+package projetoFinal.projetoFinal;
+
+public class Usuario {
+	private int cod;
+	private String nomeCompleto;
+	private String email;
+
+	public int getCod() {
+		return cod;
+	}
+
+	public void setCod(int cod) {
+		this.cod = cod;
+	}
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
