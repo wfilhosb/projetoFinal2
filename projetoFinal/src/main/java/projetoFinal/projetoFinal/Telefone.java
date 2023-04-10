eab81ebab16a9e6c7cc3722d72edd1c3f78f5c6c
wfilhosb <wfilhosb@hotmail.com>
2023-04-10 20:32:31 -0300

    Criacao da classe telefone, atributos e metodos.

---
+++ projetoFinal/src/main/java/projetoFinal/projetoFinal/Telefone.java
@@ -0,0 +1,32 @@
+package projetoFinal.projetoFinal;
+
+public class Telefone {
+	private int codigoTelefone;
+	private String numeroTelefone;
+	private String observacaoTelefone;
+
+	public int getCodigoTelefone() {
+		return codigoTelefone;
+	}
+
+	public void setCodigoTelefone(int codigoTelefone) {
+		this.codigoTelefone = codigoTelefone;
+	}
+
+	public String getNumeroTelefone() {
+		return numeroTelefone;
+	}
+
+	public void setNumeroTelefone(String numeroTelefone) {
+		this.numeroTelefone = numeroTelefone;
+	}
+
+	public String getObservacaoTelefone() {
+		return observacaoTelefone;
+	}
+
+	public void setObservacaoTelefone(String observacaoTelefone) {
+		this.observacaoTelefone = observacaoTelefone;
+	}
+
+}
