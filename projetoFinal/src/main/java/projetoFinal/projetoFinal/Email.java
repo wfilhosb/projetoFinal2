912ec2938e496c3b50d0940a5a71a162e829fa38
wfilhosb <wfilhosb@hotmail.com>
2023-04-10 20:20:13 -0300

    Criacao da classe email, atributos e metodos

---
+++ projetoFinal/src/main/java/projetoFinal/projetoFinal/Email.java
@@ -0,0 +1,32 @@
+package projetoFinal.projetoFinal;
+
+public class Email {
+	private int codigo;
+	private String endereco_email;
+	private String descricao_email;
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getEndereco_email() {
+		return endereco_email;
+	}
+
+	public void setEndereco_email(String endereco_email) {
+		this.endereco_email = endereco_email;
+	}
+
+	public String getDescricao_email() {
+		return descricao_email;
+	}
+
+	public void setDescricao_email(String descricao_email) {
+		this.descricao_email = descricao_email;
+	}
+
+}
