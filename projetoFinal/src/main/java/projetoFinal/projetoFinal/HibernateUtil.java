4d01e7ceabb032c2e48886383819a2e3905e566c
wfilhosb <wfilhosb@hotmail.com>
2023-04-17 19:56:04 -0300

    criando nosso entity manger para possibilitar CRUD

---
+++ projetoFinal/src/main/java/projetoFinal/projetoFinal/HibernateUtil.java
@@ -0,0 +1,36 @@
+package projetoFinal.projetoFinal;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class HibernateUtil {
+	public static EntityManagerFactory factory = null;
+
+	static {
+		init();
+	}
+
+	private static void init() {
+		try {
+			if (factory == null) {
+				factory = Persistence.createEntityManagerFactory("projetoFinal"); //aqui se coloca o nome do projeto
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static EntityManagerFactory getFactory() {
+		return factory;
+	}
+
+	public static EntityManager geEntityManager() {
+		return factory.createEntityManager(); /* Prove a parte de persistencia */
+	}
+
+	public static Object getPrimaryKey(Object entity) { // Retorna a primay key
+		return factory.getPersistenceUnitUtil().getIdentifier(entity);
+	}
+
+}
