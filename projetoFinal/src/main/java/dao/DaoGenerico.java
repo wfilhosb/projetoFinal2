3fd80a1f4a4ed398971655b4dbd71efd8a48d2c0
wfilhosb <wfilhosb@hotmail.com>
2023-04-24 21:10:42 -0300

    elaboramos o daoGenerico que fara o CRUD para todas as entidades

---
+++ projetoFinal/src/main/java/dao/DaoGenerico.java
@@ -0,0 +1,44 @@
+package dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import projetoFinal.projetoFinal.HibernateUtil;
+
+public class DaoGenerico<E> {
+	private EntityManager entityManager = HibernateUtil.getEntityManager();
+	
+	public void salvar(E entidade) {
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		entityManager.persist(entidade);
+		transaction.commit();
+	}
+	
+	public E atualizar(E entidade) {
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		E entidadeSalva = entityManager.merge(entidade);
+		transaction.commit();
+		return entidadeSalva;
+	}
+	
+	//esse listar trás tudo que existe cadastrado para essa classe
+	public List<E> listar(Class<E> entidade){
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		List<E> lista = entityManager.createQuery("from "+entidade.getName()).getResultList();
+		transaction.commit();
+		return lista;
+	}
+	
+	public void excluir(E entidade) {
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		entityManager.remove(entidade);
+		transaction.commit();
+	}
+	
+}
