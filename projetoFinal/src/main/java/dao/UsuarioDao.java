f59eedabab8f70ca98b0ec7962a65d42a680b14f
wfilhosb <wfilhosb@hotmail.com>
2023-05-17 22:44:35 -0300

    CRIACAO DO METODO ESPECIFICO PESQUISAR POR NOME

--- projetoFinal/src/main/java/dao/UsuarioDao.java
+++ projetoFinal/src/main/java/dao/UsuarioDao.java
@@ -1,7 +1,16 @@
 package dao;
 
+import java.util.List;
+
+import javax.persistence.Query;
+
 import model.Usuario;
 
 public class UsuarioDao<E> extends DaoGenerico<Usuario> {
-
+	
+	public List<Usuario> pesquisarPeloNome(String campoPesquisa) {
+		Query query = super.getEntityManager().createQuery("from Usuario where nomeCompleto like '%"+campoPesquisa+"%' order by nomeCompleto asc");
+		return query.getResultList();
+	}
+	
 }
